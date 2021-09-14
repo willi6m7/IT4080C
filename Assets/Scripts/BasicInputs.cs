b0fcc55f47587302f03a2cd115ad15b0de4ead49
willi6m7 <willi6m7@mail.uc.edu>
2021-09-13 21:21:49 -0400

    Movement and Camera
    Added different movement as well as a camera that follows each player

--- Assets/Scripts/BasicInputs.cs
+++ Assets/Scripts/BasicInputs.cs
@@ -1,31 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAPI;
 
-public class BasicInputs : MonoBehaviour
+public class BasicInputs : NetworkBehaviour
 {
+    public float movementSpeed = 5f;
+    public Transform camT;
+    CharacterController mpCharController;
+
+    void Start()
+    {
+        mpCharController = GetComponent<CharacterController>();
+
+        //Color change check
+        if (IsLocalPlayer)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+
+        //disable other cameras
+        if(!IsLocalPlayer)
+        {
+            camT.GetComponent<Camera>().enabled = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (IsLocalPlayer)
         {
-            transform.position += Vector3.right;
+            MPMovePlayer();
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.position += Vector3.forward;
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            transform.position += Vector3.back;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.position += new Vector3(0, 0.5f, 0);
-        }
+        
+    }
+
+    void MPMovePlayer()
+    {
+
+        Vector3 moveVect = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        mpCharController.SimpleMove(moveVect * movementSpeed);
+
     }
 }
